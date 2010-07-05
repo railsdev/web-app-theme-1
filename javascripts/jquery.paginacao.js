1b63ab5b3e08460dfa9997f62a2a4d84975d4e91
André Ferraro <aferrarobr@gmail.com>
2010-07-05 16:08:52 -0300

    Arrumado caminho do spinner.

--- javascripts/jquery.paginacao.js
+++ javascripts/jquery.paginacao.js
@@ -1,11 +1,11 @@
 $(function() {
   $(".pagination a").live("click", function() {
-    $("#carregando").html("<span class='loading'><img id='spinner' src='/images/icones/spinner.gif'>carregando...</span>");
+    $("#carregando").html("<span class='loading'><img id='spinner' src='/images/spinner.gif'>carregando...</span>");
     $.get(this.href, null, null, "script");
     return false;
   });
   $(".ordem a").live("click", function() {
-    $("#carregando").html("<span class='loading'><img id='spinner' src='/images/icones/spinner.gif'>carregando...</span>");
+    $("#carregando").html("<span class='loading'><img id='spinner' src='/images/spinner.gif'>carregando...</span>");
     $.get(this.href, null, null, "script");
     return false;
   });
